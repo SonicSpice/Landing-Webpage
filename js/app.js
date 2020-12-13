1014dfb14abaf17ed486d2cbb9e8a7e6cda1a4ea
shawn <smakimagine26@gmail.com>
2020-12-12 22:08:44 -0600

    merge

--- js/app.js
+++ js/app.js
@@ -1,28 +1,74 @@
 const linkData = [
-  { label: "Home", url: "#Home" },
-  { label: "ShoeShop", url: "#ShoeShop" },
-  { label: "Youtube", url: "#Youtube" },
-  { label: "Linkedin", url: "#Linkedin" },
-  { label: "Udacity", url: "#Udacity" },
-  { label: "Blog", url: "#Blog" },
-  { label: "WeatherApp", url: "#WeatherApp" },
-  { label: "MerryChristmas", url: "#MerryChristmas" },
-  { label: "README", url: "#README" },
+  { label: "Home", url: "HomeLink" },
+  { label: "ShoeShop", url: "ShoeShopLink" },
+  { label: "README", url: "READMELink" },
+  { label: "Linkedin", url: "LinkedinLink" },
 ];
 
 for (const link of linkData) {
   const newAnchor = document.createElement("a");
-  newAnchor.setAttribute("href", link.url);
+  newAnchor.setAttribute("id", link.url);
   newAnchor.classList.add("item");
+  if (link.label === "Home") {
+    newAnchor.classList.add("active");
+  }
   newAnchor.innerText = link.label;
   document.getElementById("navbar").appendChild(newAnchor);
 }
+
+const hambugerMenuBtn = document.getElementById("hambuger-menu-btn");
+const navbar = document.getElementById("navbar");
+
+hambugerMenuBtn.addEventListener("click", () => {
+  if (navbar.classList.contains("hideNav")) {
+    navbar.classList.remove("hideNav");
+  } else {
+    navbar.classList.add("hideNav");
+  }
+});
+
+const homelink = document.getElementById("HomeLink");
+const homesection = document.getElementById("Home");
+const shoeshoplink = document.getElementById("ShoeShopLink");
+const shoeshopsection = document.getElementById("ShoeShop");
+const linkedinlink = document.getElementById("LinkedinLink");
+const linkedinsection = document.getElementById("Linkedin");
+const readmelink = document.getElementById("READMELink");
+const readmesection = document.getElementById("README");
+
+homelink.addEventListener("click", () => {
+  window.scrollTo({
+    top: homesection.offsetTop - 48,
+    behavior: "smooth",
+  });
+});
+
+shoeshoplink.addEventListener("click", () => {
+  window.scrollTo({
+    top: shoeshopsection.offsetTop - 48,
+    behavior: "smooth",
+  });
+});
+
+linkedinlink.addEventListener("click", () => {
+  window.scrollTo({
+    top: linkedinsection.offsetTop - 48,
+    behavior: "smooth",
+  });
+});
+
+readmelink.addEventListener("click", () => {
+  window.scrollTo({
+    top: readmesection.offsetTop - 48,
+    behavior: "smooth",
+  });
+});
 
 // Get all sections that have an ID defined
 const sections = document.querySelectorAll("section[id]");
 
 // on scroll event listener
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   navHighlighter();
 });
 
@@ -40,14 +86,22 @@ function navHighlighter() {
     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
     */
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+    if (scrollY >= sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
-        .querySelector("a[href*=" + sectionId + "]")
+        .querySelector("a[id*=" + sectionId + "]")
         .classList.add("active");
+
+      document
+        .getElementById(sectionId + "-content")
+        .classList.add("bounce-animation");
     } else {
       document
-        .querySelector("a[href*=" + sectionId + "]")
+        .querySelector("a[id*=" + sectionId + "]")
         .classList.remove("active");
+
+      document
+        .getElementById(sectionId + "-content")
+        .classList.remove("bounce-animation");
     }
   });
 }
